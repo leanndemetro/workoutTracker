b0f26f50c14aae41b9adcd703b61d07b25f52d0c
leanndemetro <leanndemetro@gmail.com>
2020-10-30 20:47:09 -0400

    deploy

--- server.js
+++ server.js
@@ -13,14 +13,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false,
 });
 
 // routes
-app.use(require("./routes/api.js"));
-app.use(require("./routes/view.js"));
+app.use(require("./routes/api"));
+app.use(require("./routes/view"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
